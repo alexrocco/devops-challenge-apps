30cd59676d7b41c6f32262bb1d8fb660c386c0df
Alexandre Alvarenga <alexxrocco@gmail.com>
2019-04-09 10:23:23 -0300

    Add chart for web app

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -6,14 +6,28 @@ echo "###########################"
 echo
 
 BASE_PATH=$(pwd)
+API_CHART="api"
+WEB_CHART="web"
 
-cd ../infrastructure/database || exit 1
+deploy_api() {
+  cd ../infrastructure/database || exit 1
 
-echo "Getting database information from terraform..."
-POSTGRES_ENDPOINT=$(terraform output endpoint)
+  echo "Getting database information from terraform..."
+  terraform init
+  POSTGRES_ENDPOINT=$(terraform output endpoint)
 
-cd $BASE_PATH || exit 1
+  cd $BASE_PATH || exit 1
 
-POSTGRES_URL="postgres://app:app@${POSTGRES_ENDPOINT}/app"
+  POSTGRES_URL="postgres://app:app@${POSTGRES_ENDPOINT}/app"
 
-helm install --name api ./devops-challenge-apps-api --set postgresUrl=${POSTGRES_URL}
+  helm install --name ${API_CHART} ./devops-challenge-apps-api --set postgresUrl=${POSTGRES_URL}
+}
+
+deploy_web() {
+  API_SERVICE_NAME="http://${API_CHART}-devops-challenge-apps-api/"
+
+  helm install --name ${WEB_CHART} ./devops-challenge-apps-web --set apiHost=${API_SERVICE_NAME}
+}
+
+deploy_api
+deploy_web
