061cd12d52e14c3544a7a2bc142327117ac083e9
Alexandre Rocco Alvarenga <alexxrocco@gmail.com>
2019-04-10 13:37:01 -0300

    Add grafana + prometheus for monitoring

--- configuration/kubernetes/kubernetes_config.sh
+++ configuration/kubernetes/kubernetes_config.sh
@@ -5,7 +5,9 @@ helm init
 kubectl create clusterrolebinding add-on-cluster-admin --clusterrole=cluster-admin --serviceaccount=kube-system:default
 
 ## NGINX
-cd nginx || exit 1
-kubectl apply -f ./mandatory.yaml
-kubectl apply -f ./patch-configmap-l7.yaml
-kubectl apply -f ./service-l7.yaml
+kubectl apply -f ./nginx/mandatory.yaml
+kubectl apply -f ./nginx/patch-configmap-l7.yaml
+kubectl apply -f ./nginx/service-l7.yaml
+
+## Grafana + Prometheus - https://github.com/giantswarm/prometheus
+kubectl apply -f ./grafana+prometheus/manifests-all.yaml
